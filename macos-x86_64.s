baa1539654ab15c90baf9345e5dbccb69dd622d5
Yoshinori Takesako <takesako@gmail.com>
2026-01-11 13:54:09 +0900

    Create macos-x86_64.s
    arch -x86_64 clang macos-x86_64.s
    otool -xv a.out
    arch -x86_64 ./a.out

---
+++ macos-x86_64.s
@@ -0,0 +1,23 @@
+.section __TEXT,__text
+.globl _main
+_main:
+# 5
+    push  $2
+    push  $4
+    pop   %rdx
+# 7
+    pop   %rdi
+    push  $0x0a9ea5e7 # UTF-8
+    push  %rsp
+# 5
+    pop   %rsi
+    movl  %edi, %eax
+    bswap %eax # 02 00 00 00
+# 7
+    orl   $4, %eax
+    syscall
+    movl  %edi, %eax
+# 7
+    bswap %eax # 02 00 00 00
+    orl   $1, %eax
+    syscall
